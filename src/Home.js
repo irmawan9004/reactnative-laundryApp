600d0645d67e624e0be0a29550edbad7b5b01d97
irmawan <irmawan9001@gmail.com>
2022-02-01 15:54:56 +0700

    add fonts,header,and menu

---
+++ src/Home.js
@@ -0,0 +1,83 @@
+import {
+  Text,
+  StyleSheet,
+  View,
+  StatusBar,
+  TouchableOpacity,
+} from 'react-native';
+import React, {Component} from 'react';
+import {FlatGrid} from 'react-native-super-grid';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      jasa: [
+        {
+          namaJasa: 'Cuci Pakaian',
+          description: 'Cuci Pakaian Bersih dan Wangi',
+          icon: 'tshirt',
+        },
+        {
+          namaJasa: 'Setrika',
+          description: 'Setrika Pakaian Kering dan Wangi',
+          icon: 'fan',
+        },
+        {
+          namaJasa: 'Sepatu',
+          description: 'Cuci Sepatu Bersih dan Kinclong',
+          icon: 'shoe-prints',
+        },
+      ],
+    };
+  }
+  render() {
+    return (
+      <View style={{flex: 1}}>
+        <StatusBar barStyle="#fffff" backgroundColor="#3E61ED" />
+        <View style={{backgroundColor: '#5778FF', elevation: 5}}>
+          <Text style={styles.title}>X Laundry</Text>
+        </View>
+        <FlatGrid
+          itemDimension={130}
+          data={this.state.jasa}
+          renderItem={({item}) => (
+            <TouchableOpacity
+              style={{
+                backgroundColor: '#6886FE',
+                paddingVertical: 20,
+                justifyContent: 'center',
+                alignItems: 'center',
+                paddingHorizontal: 10,
+                borderRadius: 5,
+                elevation: 5,
+              }}>
+              <Icon name={item.icon} size={50} color="#fff" />
+              <Text style={{color: 'white', fontWeight: 'bold', marginTop: 20}}>
+                {item.namaJasa}
+              </Text>
+              <Text
+                style={{
+                  color: 'white',
+                  textAlign: 'center',
+                }}>
+                {item.description}
+              </Text>
+            </TouchableOpacity>
+          )}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 20,
+    fontFamily: 'Poppins-Black',
+    color: '#fff',
+    paddingVertical: 10,
+    textAlign: 'center',
+  },
+});
