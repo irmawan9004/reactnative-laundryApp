198a7524f51acb2d61a047ac15c1eb3523ced0ef
irmawan <irmawan9001@gmail.com>
2022-02-10 16:27:13 +0700

    admin page

--- src/Admin.js
+++ src/Admin.js
@@ -3,11 +3,23 @@ import React, {Component} from 'react';
 import {FlatGrid} from 'react-native-super-grid';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Modal from 'react-native-modal';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 export default class Admin extends Component {
   constructor(props) {
     super(props);
-    this.state = {transaksi: [], transaksiModal: false, selectedIndex: 0};
+    this.state = {
+      transaksi: [
+        {
+          nama: '',
+          harga: 0,
+          jumlah: 0,
+          icon: '',
+        },
+      ],
+      transaksiModal: false,
+      selectedIndex: 0,
+    };
   }
   componentDidMount() {
     this.getData();
@@ -45,15 +57,43 @@ export default class Admin extends Component {
               onPress={() =>
                 this.setState({transaksiModal: true, selectedIndex: index})
               }>
-              {/* <Icon name={item.icon} size={50} color="#fff" /> */}
+              <Icon name="user" size={50} color="#fff" />
               <Text style={styles.textTitle}>{item.nama}</Text>
               <Text style={styles.text}>{item.alamat}</Text>
             </TouchableOpacity>
           )}
         />
+
         <Modal isVisible={this.state.transaksiModal}>
           <View style={{flex: 1, backgroundColor: '#fff', borderRadius: 8}}>
-            <View style={{flex: 1}}></View>
+            <View style={{flex: 1}}>
+              <FlatGrid
+                itemDimension={130}
+                data={this.state.transaksi[this.state.selectedIndex].cart}
+                renderItem={({item, index}) => (
+                  <View
+                    style={{
+                      backgroundColor: '#5778FF',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      elevation: 4,
+                      paddingVertical: 20,
+                      borderRadius: 5,
+                    }}
+                    onPress={() =>
+                      this.setState({
+                        transaksiModal: true,
+                        selectedIndex: index,
+                      })
+                    }>
+                    <Icon name={item.icon} size={50} color="#fff" />
+                    <Text style={styles.textTitle}>{item.nama}</Text>
+                    <Text style={styles.text}>{item.harga}</Text>
+                    <Text style={styles.text}>{item.jumlah}</Text>
+                  </View>
+                )}
+              />
+            </View>
             <TouchableOpacity
               style={{
                 backgroundColor: 'black',
